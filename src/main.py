12285ea89d324673c758a43931bdad693583f3ea
ryngrg <garg139@purdue.edu>
2021-11-27 03:24:18 -0500

    updates

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import openai
+import os
 from io_proj import get_input_from_file
 
-openai.api_key = ""
-openai.Engine.list()
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+print(openai.Engine.list())
 prompt = get_input_from_file("../input_files/testfile.py")
 p = ""
 for line in prompt[1]:
